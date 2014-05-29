fccd006459ba372ea61fa5c849c3c59a073007f0
Fan Jin <fan.jin@mail.mcgill.ca>
2014-05-29 03:08:02 -0400

    Reorganize facebook login flow

--- vx12/server/jqueryui/js/facebook.js
+++ vx12/server/jqueryui/js/facebook.js
@@ -36,42 +36,7 @@ function checkLoginState() {
   });
 }
 
-window.fbAsyncInit = function() {
-FB.init({
-  appId      : '287835298059518',
-  cookie     : true,  // enable cookies to allow the server to access 
-                      // the session
-  xfbml      : true,  // parse social plugins on this page
-  version    : 'v2.0' // use version 2.0
-});
 
-// Now that we've initialized the JavaScript SDK, we call 
-// FB.getLoginStatus().  This function gets the state of the
-// person visiting this page and can return one of three states to
-// the callback you provide.  They can be:
-//
-// 1. Logged into your app ('connected')
-// 2. Logged into Facebook, but not your app ('not_authorized')
-// 3. Not logged into Facebook and can't tell if they are logged into
-//    your app or not.
-//
-// These three cases are handled in the callback function.
-
-FB.getLoginStatus(function(response) {
-  stage = 'onload'
-  statusChangeCallback(response, stage);
-});
-
-};
-
-// Load the SDK asynchronously
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=287835298059518&version=v2.0";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
 
 // Here we run a very simple test of the Graph API after login is
 // successful.  See statusChangeCallback() for when this call is made.
